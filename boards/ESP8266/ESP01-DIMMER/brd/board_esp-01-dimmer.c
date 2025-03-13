6821a29e21468de574219d05700e3090b42ba3d7
kuba <qb4.dev@gmail.com>
2025-03-13 21:21:39 +0100

    ESP01-DIMMER added board v1.2 support

--- boards/ESP8266/ESP01-DIMMER/brd/board_esp-01-dimmer.c
+++ boards/ESP8266/ESP01-DIMMER/brd/board_esp-01-dimmer.c
@@ -5,10 +5,9 @@
 
 #include <board.h>
 #include <pca9557.h>
+#include <generic-input.h>
 #include <exp-input.h>
 #include <ledc-channel.h>
-
-#ifdef CONFIG_BSP_ESP01_DIMMER_v1_1
 
 #define IN1_SETTINGS_GR "IN1"
 #define IN2_SETTINGS_GR "IN2"
@@ -65,22 +64,49 @@ static const settings_group_t board_settings[] = {
     {}
 };
 
+#ifdef CONFIG_BSP_ESP01_DIMMER_v1_0
+// Global variables for board v1.0
+static bsp_t brd_esp01_dimmer = { .id = "ESP01_DIMMER",
+                                  .ver = "1.0",
+                                  .settings_pack = board_settings };
+
+static supla_channel_t *ledc_channel;
+static supla_channel_t *input1_channel;
+static supla_channel_t *input2_channel;
+#endif //CONFIG_BSP_ESP01_DIMMER_v1_0
+
+#ifdef CONFIG_BSP_ESP01_DIMMER_v1_1
+// Global variables for board v1.1
 static bsp_t brd_esp01_dimmer = { .id = "ESP01_DIMMER",
                                   .ver = "1.1",
                                   .settings_pack = board_settings };
 
-bsp_t *const bsp = &brd_esp01_dimmer;
-
-static i2c_dev_t pca9536;
-
+static i2c_dev_t        pca9536;
 static supla_channel_t *input0_channel;
 static supla_channel_t *input1_channel;
 static supla_channel_t *input2_channel;
 static supla_channel_t *ledc_channel;
+#endif //CONFIG_BSP_ESP01_DIMMER_v1_1
+
+#ifdef CONFIG_BSP_ESP01_DIMMER_v1_2
+// Global variables for board v1.2
+static bsp_t            brd_esp01_dimmer = { .id = "ESP01_DIMMER",
+                                             .ver = "1.2",
+                                             .settings_pack = board_settings };
+static i2c_dev_t        pca9536;
+static supla_channel_t *input0_channel;
+static supla_channel_t *input1_channel;
+static supla_channel_t *input2_channel;
+static supla_channel_t *ledc_channel;
+#endif //CONFIG_BSP_ESP01_DIMMER_v1_2
+
+bsp_t *const bsp = &brd_esp01_dimmer;
 
 esp_err_t board_early_init(void)
 {
     esp_set_cpu_freq(ESP_CPU_FREQ_80M);
+    settings_nvs_read(bsp->settings_pack);
+    //settings_pack_print(bsp->settings_pack);
     return ESP_OK;
 }
 
@@ -128,6 +154,118 @@ static bool pause_is_active(void)
     return time_is_between(from->time.hh, from->time.mm, to->time.hh, to->time.mm);
 }
 
+static void pulse_task(void *arg)
+{
+    TSD_SuplaChannelNewValue new_value = {};
+    TRGBW_Value             *rgbw = (TRGBW_Value *)&new_value.value;
+    EventBits_t              bits;
+
+    while (1) {
+        bits = device_get_event_bits();
+        if (bits & DEVICE_CONFIG_EVENT_BIT) {
+            rgbw->brightness = rgbw->brightness ? 0 : 100;
+            supla_ledc_channel_set_brightness(ledc_channel, &new_value);
+        }
+        vTaskDelay(pdMS_TO_TICKS(1500));
+    }
+}
+
+#if defined CONFIG_BSP_ESP01_DIMMER_v1_0
+static void detect_cb(gpio_num_t pin_num, input_event_t event, void *arg)
+{
+    const char *events[] = { [INPUT_EVENT_INIT] = "INPUT_EVENT_INIT",
+                             [INPUT_EVENT_CLICKx1] = "INPUT_EVENT_CLICKx1",
+                             [INPUT_EVENT_CLICKx2] = "INPUT_EVENT_CLICKx2",
+                             [INPUT_EVENT_CLICKx3] = "INPUT_EVENT_CLICKx3",
+                             [INPUT_EVENT_CLICKx4] = "INPUT_EVENT_CLICKx4",
+                             [INPUT_EVENT_CLICKx5] = "INPUT_EVENT_CLICKx5",
+                             [INPUT_EVENT_DONE] = "INPUT_EVENT_DONE" };
+
+    TSD_SuplaChannelNewValue new_value = {};
+    TRGBW_Value             *rgbw = (TRGBW_Value *)&new_value.value;
+    uint8_t                  brightness;
+    EventBits_t              bits = device_get_event_bits();
+
+    const char *gr = (pin_num == GPIO_NUM_0) ? IN1_SETTINGS_GR :
+                     (pin_num == GPIO_NUM_2) ? IN2_SETTINGS_GR :
+                                               NULL;
+
+    setting_t *off_delay_set = settings_pack_find(bsp->settings_pack, gr, "OFF_DELAY");
+    setting_t *reduced_br_set = settings_pack_find(bsp->settings_pack, REDUCTION_GR, "BR");
+
+    ESP_LOGI(TAG, "input event GPIO %d: %s", pin_num, events[event]);
+    switch (event) {
+    case INPUT_EVENT_INIT:
+        if (!(bits & DEVICE_CONFIG_EVENT_BIT)) {
+            if (pause_is_active()) {
+                ESP_LOGW(TAG, "pause active");
+                break;
+            }
+
+            if (brightness_reduction_is_active()) {
+                rgbw->brightness = reduced_br_set ? reduced_br_set->num.val : 100;
+                ESP_LOGW(TAG, "reduced brightness to %d", rgbw->brightness);
+            } else {
+                rgbw->brightness = 100;
+            }
+            supla_ledc_channel_set_brightness(ledc_channel, &new_value);
+        }
+        break;
+    case INPUT_EVENT_CLICKx3:
+        if (!(bits & DEVICE_CONFIG_EVENT_BIT))
+            device_init_config();
+        else
+            device_exit_config();
+        break;
+    case INPUT_EVENT_DONE:
+        supla_ledc_channel_get_brightness(ledc_channel, &brightness);
+        if (brightness) {
+            new_value.DurationMS = off_delay_set ? 1000 * off_delay_set->num.val : 5000;
+            rgbw->brightness = brightness;
+            supla_ledc_channel_set_brightness(ledc_channel, &new_value);
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+esp_err_t board_supla_init(supla_dev_t *dev)
+{
+    struct ledc_channel_config ledc_channel_conf = { .gpio = GPIO_NUM_3,
+                                                     .ledc_channel = LEDC_CHANNEL_0,
+                                                     .fade_time = 1000 };
+    ledc_channel = supla_ledc_channel_create(&ledc_channel_conf);
+
+    struct generic_input_config in1_conf = { .gpio = GPIO_NUM_0,
+                                             .action_trigger_caps = SUPLA_ACTION_CAP_TURN_ON |
+                                                                    SUPLA_ACTION_CAP_TURN_OFF,
+                                             .related_channel = &ledc_channel,
+                                             .on_detect_cb = detect_cb,
+                                             .arg = ledc_channel };
+    struct generic_input_config in2_conf = { .gpio = GPIO_NUM_2,
+                                             .action_trigger_caps = SUPLA_ACTION_CAP_TURN_ON |
+                                                                    SUPLA_ACTION_CAP_TURN_OFF,
+                                             .related_channel = &ledc_channel,
+                                             .on_detect_cb = detect_cb,
+                                             .arg = ledc_channel };
+
+    input1_channel = supla_generic_input_create(&in1_conf);
+    input2_channel = supla_generic_input_create(&in2_conf);
+
+    supla_dev_set_name(dev, bsp->id);
+    supla_dev_add_channel(dev, ledc_channel);
+    supla_dev_add_channel(dev, input1_channel);
+    supla_dev_add_channel(dev, input2_channel);
+
+    xTaskCreate(&pulse_task, "pulse", 2048, NULL, 1, NULL);
+    ESP_LOGI(TAG, "board init completed OK");
+    return ESP_OK;
+}
+
+#endif //CONFIG_BSP_ESP01_DIMMER_v1_0
+
+#if defined CONFIG_BSP_ESP01_DIMMER_v1_1 || defined CONFIG_BSP_ESP01_DIMMER_v1_2
 static void config_btn_calback(gpio_num_t pin_num, exp_input_event_t event, void *arg)
 {
     TSD_SuplaChannelNewValue new_value = {};
@@ -206,28 +344,9 @@ static void input_calback(gpio_num_t pin_num, exp_input_event_t event, void *arg
     }
 }
 
-static void pulse_task(void *arg)
-{
-    TSD_SuplaChannelNewValue new_value = {};
-    TRGBW_Value             *rgbw = (TRGBW_Value *)&new_value.value;
-    EventBits_t              bits;
-
-    while (1) {
-        bits = device_get_event_bits();
-        if (bits & DEVICE_CONFIG_EVENT_BIT) {
-            rgbw->brightness = rgbw->brightness ? 0 : 100;
-            supla_ledc_channel_set_brightness(ledc_channel, &new_value);
-        }
-        vTaskDelay(pdMS_TO_TICKS(1500));
-    }
-}
-
 esp_err_t board_supla_init(supla_dev_t *dev)
 {
     setting_t *active_lvl_set = NULL;
-
-    settings_nvs_read(bsp->settings_pack);
-    //settings_pack_print(bsp->settings_pack);
 
     ESP_ERROR_CHECK(i2cdev_init());
     ESP_ERROR_CHECK(
@@ -281,6 +400,7 @@ esp_err_t board_supla_init(supla_dev_t *dev)
     ESP_LOGI(TAG, "board init completed OK");
     return ESP_OK;
 }
+#endif // CONFIG_BSP_ESP01_DIMMER_v1_1 || CONFIG_BSP_ESP01_DIMMER_v1_2
 
 esp_err_t board_on_config_mode_init(void)
 {
@@ -297,5 +417,3 @@ esp_err_t board_on_config_mode_exit(void)
     supla_ledc_channel_set_brightness(ledc_channel, &new_value);
     return ESP_OK;
 }
-
-#endif
