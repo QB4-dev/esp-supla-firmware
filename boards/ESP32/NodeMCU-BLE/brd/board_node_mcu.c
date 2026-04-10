8fc21deff1be6d4a0ed0529732e6e4a669b9d496
kuba <qb4.dev@gmail.com>
2026-04-10 21:54:42 +0200

    Add LampSmart BLE channel support and configuration - Introduced new LEDSmart BLE channel implementation with configuration options. - Updated project settings and partition table for BLE support. - Modified CMakeLists to include the new BLE component. - Refactored existing code to integrate BLE functionality.

--- boards/ESP32/NodeMCU-BLE/brd/board_node_mcu.c
+++ boards/ESP32/NodeMCU-BLE/brd/board_node_mcu.c
@@ -5,31 +5,75 @@
 
 #include <board.h>
 #include <button.h>
-#include <binary-sensor.h>
+#include <ledsmart-ble-channel.h>
 #include <relay-channel.h>
 
 #ifdef CONFIG_BSP_ESP32
 
 static const char *TAG = "BSP";
 
-static const settings_group_t board_settings_pack[] = { {} };
+#define BLE_SETTINGS_GR "BLE"
 
-static bsp_t brd_nodemcu = { .id = "ESP32-BLE", .ver = "1.0", .settings_pack = board_settings_pack };
+static const char *protocol_labels[] = { [LAMPSMART_VARIANT_3] = "V3",
+                                         [LAMPSMART_VARIANT_2] = "V2",
+                                         [LAMPSMART_VARIANT_1A] = "V1A",
+                                         [LAMPSMART_VARIANT_1B] = "V1B",
+                                         NULL };
+
+static setting_t ble_settings[] = {
+    { .id = "PROTO",
+      .label = "PROTOCOL",
+      .type = SETTING_TYPE_ONEOF,
+      .oneof = { 0, 0, protocol_labels } },
+    { .id = "GROUP_ID",
+      .label = "GROUP ID",
+      .type = SETTING_TYPE_NUM,
+      .num = { 0, 0, { 1, 1024 } } },
+    {} //last element
+};
+
+static const settings_group_t board_settings_pack[] = {
+    { .id = BLE_SETTINGS_GR, .label = "BLE settings", .settings = ble_settings },
+    {} //last element
+};
+
+static bsp_t brd_nodemcu = { .id = "ESP32-BLE",
+                             .ver = "1.0",
+                             .settings_pack = board_settings_pack };
 
 bsp_t *const bsp = &brd_nodemcu;
 
-static supla_channel_t *input1_channel;
-// static supla_channel_t *input2_channel;
-static supla_channel_t *relay_channel;
-static supla_dev_t     *supla_dev;
+static supla_channel_t *ble_channel;
+
+static esp_err_t settings_handler(const settings_group_t *settings, void *arg)
+{
+    lampsmart_variant_t variant;
+    uint32_t            group_id;
+
+    supla_channel_t *ch = arg;
+
+    setting_t *proto_set = settings_pack_find(bsp->settings_pack, BLE_SETTINGS_GR, "PROTO");
+    setting_t *group_set = settings_pack_find(bsp->settings_pack, BLE_SETTINGS_GR, "GROUP_ID");
+
+    variant = proto_set ? proto_set->oneof.val : LAMPSMART_VARIANT_3;
+    group_id = group_set ? group_set->num.val : 0x1234;
+
+    lamp_ble_channel_set_protocol(ch, variant);
+    lamp_ble_channel_set_group_id(ch, group_id);
+
+    ESP_LOGI(TAG, "Settings updated: pairing protocol=%s group_id=0x%08" PRIX32,
+             protocol_labels[variant], group_id);
+    lamp_ble_channel_pair(ble_channel);
+    return ESP_OK;
+}
 
 static void button_cb(button_t *btn, button_state_t state)
 {
     EventBits_t bits = device_get_event_bits();
     switch (state) {
     case BUTTON_CLICKED:
-        ESP_LOGI(TAG, "btn clicked");
-        supla_dev_send_notification(supla_dev, -1, "NodeMCU", "PUSH notification", 0);
+        ESP_LOGI(TAG, "btn clicked - pairing BLE channel");
+        lamp_ble_channel_pair(ble_channel);
         break;
     case BUTTON_PRESSED_LONG:
         ESP_LOGI(TAG, "btn pressed long");
@@ -47,25 +91,22 @@ static button_t btn = { .gpio = GPIO_NUM_0, .callback = button_cb };
 
 esp_err_t board_early_init(void)
 {
+    settings_nvs_read(bsp->settings_pack);
+    settings_pack_print(bsp->settings_pack);
+    button_init(&btn);
     return ESP_OK;
 }
 
 esp_err_t board_supla_init(supla_dev_t *dev)
 {
-
-    struct relay_channel_config relay_channel_conf = {
-        .gpio = GPIO_NUM_2,
-        .default_function = SUPLA_CHANNELFNC_POWERSWITCH,
-        .supported_functions = SUPLA_BIT_FUNC_POWERSWITCH | SUPLA_BIT_FUNC_LIGHTSWITCH | SUPLA_BIT_FUNC_STAIRCASETIMER
+    struct lamp_ble_channel_config ble_channel_conf = {
+        .lamp_config = LAMPSMART_BLE_CONFIG_DEFAULT(),
     };
 
-    button_init(&btn);
-    relay_channel = supla_relay_channel_create(&relay_channel_conf);
-    supla_dev_add_channel(dev, relay_channel);
+    ble_channel = lamp_ble_channel_create(&ble_channel_conf);
+    supla_dev_add_channel(dev, ble_channel);
 
-    supla_dev_enable_notifications(dev, 0x00);
-
-    supla_dev = dev; //store pointer
+    settings_handler_register(settings_handler, ble_channel);
     ESP_LOGI(TAG, "board init completed OK");
     return ESP_OK;
 }
---
+++ components/supla-outputs/ledsmart-ble-channel.c
@@ -0,0 +1,191 @@
+/*
+ * Copyright (c) 2025 <qb4.dev@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#include "include/ledsmart-ble-channel.h"
+#include <stdlib.h>
+#include <esp_log.h>
+#include <esp_timer.h>
+#include <esp-supla.h>
+
+static const char *TAG = "BLE-CH";
+
+struct lamp_ble_nvs_state {
+    int active_func;
+};
+
+struct lamp_ble_channel_data {
+    struct lamp_ble_channel_config config;
+    struct lamp_ble_nvs_state      nvs_state;
+    lampsmart_ble_t                light;
+};
+
+static const char *variant_name(lampsmart_variant_t variant)
+{
+    switch (variant) {
+    case LAMPSMART_VARIANT_3:
+        return "V3";
+    case LAMPSMART_VARIANT_2:
+        return "V2";
+    case LAMPSMART_VARIANT_1A:
+        return "V1A";
+    case LAMPSMART_VARIANT_1B:
+        return "V1B";
+    default:
+        return "UNKNOWN";
+    }
+}
+
+static int lamp_ble_channel_init(supla_channel_t *ch)
+{
+    struct lamp_ble_channel_data *data = supla_channel_get_data(ch);
+    const int                     ch_num = supla_channel_get_assigned_number(ch);
+    esp_err_t                     rc;
+
+    ESP_LOGI(TAG, "ch[%d] init", ch_num);
+    rc = supla_esp_nvs_channel_state_restore(ch, &data->nvs_state, sizeof(data->nvs_state));
+    if (rc == ESP_OK) {
+        ESP_LOGI(TAG, "ch[%d] nvs read OK:func=%d", ch_num, data->nvs_state.active_func);
+        supla_channel_set_active_function(ch, data->nvs_state.active_func);
+    }
+    return SUPLA_RESULTCODE_TRUE;
+}
+
+static int lamp_ble_channel_on_cfg_recv(supla_channel_t *ch, TSD_ChannelConfig *config)
+{
+    struct lamp_ble_channel_data *data = supla_channel_get_data(ch);
+    ESP_LOGI(TAG, "ch[%d] got config fn=%d", config->ChannelNumber, config->Func);
+
+    switch (config->Func) {
+    case SUPLA_CHANNELFNC_DIMMER:
+    case SUPLA_CHANNELFNC_DIMMER_CCT:
+        data->nvs_state.active_func = config->Func;
+        supla_esp_nvs_channel_state_store(ch, &data->nvs_state, sizeof(data->nvs_state));
+        break;
+    default:
+        break;
+    }
+    return ESP_OK;
+}
+
+int lamp_ble_channel_set_value(supla_channel_t *ch, TSD_SuplaChannelNewValue *new_value)
+{
+    struct lamp_ble_channel_data *data = supla_channel_get_data(ch);
+
+    const int ch_num = supla_channel_get_assigned_number(ch);
+    const int active_func = data->nvs_state.active_func;
+
+    TRGBW_Value *rgbw = (TRGBW_Value *)new_value->value;
+
+    uint8_t br = rgbw->brightness;
+    uint8_t wt = rgbw->whiteTemperature;
+    uint8_t cold, warm;
+
+    switch (active_func) {
+    case SUPLA_CHANNELFNC_DIMMER: {
+        uint8_t val = (uint16_t)br * 255 / 100;
+        if (rgbw->brightness == 0)
+            lampsmart_ble_turn_off(data->light);
+        else
+            lampsmart_ble_set_levels(data->light, val, val);
+    } break;
+    case SUPLA_CHANNELFNC_DIMMER_CCT:
+        /* cold/warm balance */
+        cold = (uint16_t)(wt * 0xFF / 100);
+        warm = (uint16_t)((100 - wt) * 0xFF / 100);
+        /* scale by brightness */
+        cold = (uint16_t)(cold * br / 100);
+        warm = (uint16_t)(warm * br / 100);
+        if (rgbw->brightness == 0)
+            lampsmart_ble_turn_off(data->light);
+        else
+            lampsmart_ble_set_levels(data->light, cold, warm);
+
+        ESP_LOGI(TAG, "ch[%d] val: BR=%d WT=%d cold=%d warm=%d", ch_num, rgbw->brightness,
+                 rgbw->whiteTemperature, cold, warm);
+        break;
+    default:
+        break;
+    }
+    return supla_channel_set_rgbw_value(ch, rgbw);
+}
+
+supla_channel_t *lamp_ble_channel_create(const struct lamp_ble_channel_config *conf)
+{
+    struct lamp_ble_channel_data *data;
+
+    const supla_channel_config_t supla_channel_config = {
+        .type = SUPLA_CHANNELTYPE_DIMMER,
+        .supported_functions = SUPLA_RGBW_BIT_FUNC_DIMMER | SUPLA_RGBW_BIT_FUNC_DIMMER_CCT,
+        .default_function = SUPLA_CHANNELFNC_DIMMER_CCT,
+        .flags = SUPLA_CHANNEL_FLAG_CHANNELSTATE,
+        .on_channel_init = lamp_ble_channel_init,
+        .on_set_value = lamp_ble_channel_set_value,
+        .on_config_recv = lamp_ble_channel_on_cfg_recv
+    };
+
+    supla_channel_t *ch = supla_channel_create(&supla_channel_config);
+    if (!ch)
+        return NULL;
+
+    data = malloc(sizeof(struct lamp_ble_channel_data));
+    if (!data) {
+        supla_channel_free(ch);
+        return NULL;
+    }
+
+    data->config = *conf;
+    supla_channel_set_data(ch, data);
+
+    if (lampsmart_ble_init(&data->light, &data->config.lamp_config) != ESP_OK) {
+        supla_channel_free(ch);
+        free(data);
+        return NULL;
+    }
+
+    ESP_LOGI(TAG,
+             "LampSmart controller initialized for group: 0x%08" PRIX32
+             " variant=%s duration_ms=%" PRIu32,
+             data->config.lamp_config.group_id, variant_name(data->config.lamp_config.variant),
+             data->config.lamp_config.tx_duration_ms);
+    return ch;
+}
+
+int lamp_ble_channel_set_protocol(supla_channel_t *ch, lampsmart_variant_t variant)
+{
+    struct lamp_ble_channel_data *data = supla_channel_get_data(ch);
+    const int                     ch_num = supla_channel_get_assigned_number(ch);
+
+    lampsmart_ble_config_t cfg;
+    if (lampsmart_ble_get_config(data->light, &cfg) == ESP_OK) {
+        cfg.variant = variant;
+        ESP_LOGI(TAG, "ch[%d] set protocol variant %s", ch_num, variant_name(variant));
+        return lampsmart_ble_set_config(data->light, &cfg);
+    }
+    return ESP_ERR_INVALID_STATE;
+}
+
+int lamp_ble_channel_set_group_id(supla_channel_t *ch, uint32_t group_id)
+{
+    struct lamp_ble_channel_data *data = supla_channel_get_data(ch);
+    const int                     ch_num = supla_channel_get_assigned_number(ch);
+    lampsmart_ble_config_t        cfg;
+    if (lampsmart_ble_get_config(data->light, &cfg) == ESP_OK) {
+        cfg.group_id = group_id;
+        ESP_LOGI(TAG, "ch[%d] set group_id 0x%08" PRIX32, ch_num, group_id);
+        return lampsmart_ble_set_config(data->light, &cfg);
+    }
+    return ESP_ERR_INVALID_STATE;
+}
+
+int lamp_ble_channel_pair(supla_channel_t *ch)
+{
+    struct lamp_ble_channel_data *data = supla_channel_get_data(ch);
+    const int                     ch_num = supla_channel_get_assigned_number(ch);
+
+    ESP_LOGI(TAG, "ch[%d] BLE  pairing started using SmartLamp protocol %s", ch_num,
+             variant_name(data->config.lamp_config.variant));
+    return lampsmart_ble_pair(data->light);
+}
